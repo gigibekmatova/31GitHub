a577b60bf5fb8e3a39b848d7c87c2d3795f3e65e
Gulnaz <gigibekmatova@mail.ru>
2023-11-12 18:19:52 -0800

    Time: 55 ms (65.31%), Space: 41.8 MB (58.08%) - LeetHub

---
+++ 2637-promise-time-limit/2637-promise-time-limit.js
@@ -0,0 +1,29 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+    
+ return async function(...args) {
+        return new Promise((resolve, reject) => {
+            const timer = setTimeout(() => {
+                reject("Time Limit Exceeded")
+            }, t)
+
+            fn(...args)
+                .then((result) => {
+                    clearTimeout(timer)
+                    resolve(result)
+                })
+                .catch((err) => {
+                    clearTimeout(timer)
+                    reject(err)
+                })
+        })
+    }
+};
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
