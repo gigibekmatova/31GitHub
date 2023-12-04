ef15e10ef34b5772e492c11233beb09fe4635c3d
Gulnaz <gigibekmatova@mail.ru>
2023-12-03 20:06:32 -0800

    Time: 73 ms (7.70%), Space: 42.5 MB (51.90%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const leftSymbols = [];
+    for (let i = 0; i < s.length; i++) {
+    if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+            leftSymbols.push(s[i]);
+        }
+        else if (s[i] === ')' && leftSymbols.length !== 0 && leftSymbols[leftSymbols.length - 1] === '(') {
+            leftSymbols.pop();
+        } else if (s[i] === '}' && leftSymbols.length !== 0 && leftSymbols[leftSymbols.length - 1] === '{') {
+            leftSymbols.pop();
+        } else if (s[i] === ']' && leftSymbols.length !== 0 && leftSymbols[leftSymbols.length - 1] === '[') {
+            leftSymbols.pop();
+        }
+        else {
+            return false;
+        }
+    }
+    return leftSymbols.length === 0;
+};
