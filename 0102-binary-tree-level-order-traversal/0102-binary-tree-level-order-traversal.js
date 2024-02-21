08287771a9e0b2b639c66a929226f80991279c1d
Gulnaz <gulnazbekmatova8@gmail.com>
2024-02-21 09:33:47 -0800

    Time: 80 ms (35.50%), Space: 54.6 MB (34.04%) - LeetHub

---
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+    if (!root) return [];
+
+  var ans = []
+    , tmp = [root];
+
+  while (tmp.length) {
+    var res = []
+      , a = [];
+
+    for (var i = 0, len = tmp.length; i < len; i++) {
+      if (!tmp[i]) continue;
+      res.push(tmp[i].val);
+      a.push(tmp[i].left);
+      a.push(tmp[i].right);
+    }
+
+    tmp = a.concat();
+    if (res.length)
+      ans.push(res);
+  }
+
+  return ans;
+};
