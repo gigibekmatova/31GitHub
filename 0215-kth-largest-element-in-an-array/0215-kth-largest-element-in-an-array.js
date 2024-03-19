a80bdccd9a596b3abe76300bb39b5039e592d063
Gulnaz <gulnazbekmatova8@gmail.com>
2024-03-18 18:18:41 -0700

    Time: 149 ms (46.30%), Space: 58.8 MB (86.62%) - LeetHub

---
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function(nums, k) {
+  nums.sort(function(a, b) {
+    return b - a;
+  });
+  
+  return nums[k - 1];
+};
