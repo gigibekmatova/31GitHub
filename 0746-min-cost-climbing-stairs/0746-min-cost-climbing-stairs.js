ae0f9cee902f483859e522c28d7f34d52115973c
Gigi <gulnazbekmatova8@gmail.com>
2024-05-27 19:50:48 -0700

    Time: 56 ms (60.35%), Space: 49.2 MB (88.96%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function(cost) {
+   for (var i = 2; i < cost.length; i++) {
+       cost[i] = Math.min(cost[i-2] + cost[i], cost[i-1]+cost[i]);
+   } 
+    return Math.min(cost[cost.length-1], cost[cost.length-2]);
+};
