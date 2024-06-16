bc1e25c787f9a2f5455b8fa5d6c4a80bc871fa5b
Gigi <gulnazbekmatova8@gmail.com>
2024-06-16 16:43:48 -0700

    Time: 523 ms (5.02%), Space: 72.2 MB (85.54%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures = function(temperatures) {
+   let hash = []
+  let ans = []
+
+  for (let i = temperatures.length; i--; ) {
+    let temperature = temperatures[i]
+    ans[i] = Infinity
+
+    for (let j = temperature + 1; j <= 100; j++) {
+      if (hash[j] && hash[j] - i < ans[i]) {
+        ans[i] = hash[j] - i
+      }
+    }
+
+    if (ans[i] === Infinity) ans[i] = 0
+    hash[temperature] = i
+  }
+
+  return ans 
+};
