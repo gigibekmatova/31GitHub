22b0710922c2a3fb523838758cd8c66d5c812b2c
Gigi <gulnazbekmatova8@gmail.com>
2024-11-01 22:43:26 -0700

    Time: 6 ms (88.39%), Space: 74.4 MB (79.02%) - LeetHub

---
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+var pairSum = function(head) {
+   let slow = head;
+        let fast = head;
+
+        // Get middle of the linked list.
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        // Reverse second half of the linked list.
+        let nextNode, prev = null;
+        while (slow != null) {
+            nextNode = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = nextNode;
+        }
+
+        let start = head;
+        let maximumSum = 0;
+        while (prev != null) {
+            maximumSum = Math.max(maximumSum, start.val + prev.val);
+            prev = prev.next;
+            start = start.next;
+        }
+
+        return maximumSum; 
+};
